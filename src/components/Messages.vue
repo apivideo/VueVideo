0bf9573370a714c41f3316f76b8d30c8bfe8b776
emesdg <emesdg@gmail.com>
2020-03-15 15:58:21 +0200

    cleaned demo

---
+++ src/components/Messages.vue
@@ -0,0 +1,18 @@
+<template>
+        <div>
+            <h4>Messages</h4>
+            <ul>
+                <li v-for="message in messages">{{message}}</li>            
+            </ul>
+        </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            messages: ["Hello", "Hi", "working"]
+        };
+    }
+};
+</script>

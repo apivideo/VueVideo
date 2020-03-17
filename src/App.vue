dd3b9b76c31116a21f03638d4024a034907367c9
Yitzi Ginzberg <emesdg@gmail.com>
2020-03-17 14:12:21 +0200

    cloak

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,42 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-     
-    </v-app-bar>
+    <v-app>
+        <v-app-bar
+                app
+                color="primary"
+                dark
+        >
 
-    <v-content>
-      <br>
-      <Authenticate/>
-      <br>
-      <Videos/>
-    </v-content>
-  </v-app>
+        </v-app-bar>
+
+        <v-content>
+            <br>
+            <Authenticate/>
+            <br>
+            <Videos v-cloak v-if="false"/>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import Videos from './components/Videos';
-import Authenticate from './components/Authenticate';
+    import Videos from './components/Videos';
+    import Authenticate from './components/Authenticate';
 
-export default {
-  name: 'App',
+    export default {
+        name: 'App',
 
-  components: {
-    Videos,
-    Authenticate
-  },
+        components: {
+            Videos,
+            Authenticate
+        },
 
-  data: () => ({
-    //
-  }),
-};
+        data: () => ({
+            //
+        }),
+    };
 </script>
+
+<style>
+    [v-cloak] {
+        display: none;
+    }
+</style>
--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -1,40 +1,41 @@
 <template>
-  <v-card class="mx-auto" max-width="400" tile>
-    <v-toolbar dark>
-      <v-toolbar-title>Videos</v-toolbar-title>
-    </v-toolbar>
-    <v-container>
-      <v-row>
-        <v-col>
-          <v-list>
-            <v-list-item-group>
-              <v-list-item v-for="(message, i) in messages" :key="i">
-                <v-list-item-content>
-                  <v-list-item-title v-html="message"></v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
+    <v-card class="mx-auto" max-width="400" tile>
+        <v-toolbar dark>
+            <v-toolbar-title>Videos</v-toolbar-title>
+        </v-toolbar>
+        <v-container>
+            <v-row>
+                <v-col>
+                    <v-list>
+                        <v-list-item-group>
+                            <v-list-item v-for="(message, i) in messages" :key="i">
+                                <v-list-item-content>
+                                    <v-list-item-title v-html="message"></v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-card>
 </template>
 
 <script>
-import axios from "axios";
+    import axios from "axios";
 
-export default {
-  data() {
-    return {
-      messages: []
+    export default {
+        data() {
+            return {
+                messages: []
+            };
+        },
+        created: function () {
+          // eslint-disable-next-line no-global-assign
+            self = this;
+            axios.get("http://localhost:3000/messages").then(function (response) {
+                self.messages = response.data;
+            });
+        }
     };
-  },
-  created: function() {
-    self = this;
-    axios.get("http://localhost:3000/messages").then(function(response) {
-      self.messages = response.data;
-    });
-  }
-};
 </script>

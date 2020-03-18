f1a07a7c0ce01e0234c481456a5eae6e4ba23241
Yitzi Ginzberg <emesdg@gmail.com>
2020-03-18 15:58:29 +0200

    removal buttons

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -7,11 +7,12 @@
             <v-row>
                 <v-col>
                     <v-list>
-                        <v-list-item-group>
-                            <v-list-item v-for="(video, index) in videos" :key="index">
+                        <v-list-item-group is="transition-group" name="neo-list">
+                            <v-list-item v-for="(video, index) in videos" :key="index" :class="{'shadow-sm': true}">
                                 <v-list-item-content>
                                     <v-list-item-title>{{video.title}}</v-list-item-title>
                                 </v-list-item-content>
+                                <v-btn @click="remove(index)">remove</v-btn>
                             </v-list-item>
                         </v-list-item-group>
                     </v-list>
@@ -36,6 +37,22 @@
                     this.videos = response.data.data;
                 });
             });
+        },
+        methods: {
+            remove: function (index) {
+                this.videos.splice(index, 1);
+            }
         }
     };
 </script>
+
+<style scoped>
+    .neo-list-leave-to, .neo-list-enter {
+        opacity: 0;
+        transform: translateY(30px);
+    }
+
+    .neo-list-enter-active, .neo-list-leave-active {
+        transition: all 1s linear;
+    }
+</style>
